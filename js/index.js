fd612fa40d7b921280d5eb869714f8fbf6a8d85c
Damian Ponce <dponcez_git@hotmail.com>
2022-12-11 00:52:53 -0500

    Chore: Update files

--- js/index.js
+++ js/index.js
@@ -1,139 +1,144 @@
-function initApp(){
-  const TIMER = {
-    pomodoro: 25,
-    shortBreak: 5,
-    longBreak: 15,
-    longBreakInterval: 4,
-    sessions: 0
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
+  sessions: 0
+}
+
+const handleMode = (event) => {
+  const { mode } = event.target.dataset;
+  if(!mode) return;
+
+  switchMode(mode);
+  stopTimer();
+}
+
+const sounds = new Audio('audio/button-sound.mp3')
+const modeButtons = document.getElementById('js_mode-buttons');
+modeButtons.addEventListener('click', handleMode);
+
+const switchMode = (mode) => {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0
   }
 
-  const MODE_BUTTONS = document.getElementById('js_mode-buttons');
-  MODE_BUTTONS.addEventListener('click', handleMode);
+  document.querySelectorAll('button[data-mode]')
+    .forEach(button => button.classList.remove('active'));
+  document.querySelector(`[data-mode="${mode}"]`)
+    .classList.add('active');
+  document.body.style.color = `var(--${mode === 'pomodoro' ? 'white' : 'dark'})`;
+  document.body.style.backgroundImage = `var(--${mode})`;
+  document.getElementById('js-progress')
+    .setAttribute('max', timer.remainingTime.total);
 
-  function handleMode(event){
-    const { mode } = event.target.dataset;
+  updateTimer()
+}
 
-    if(!mode) return;
+const updateTimer = () => {
+  const { remainingTime } = timer;
+  const remainingMins = `${remainingTime.minutes}`.padStart(2, '0');
+  const remainingSecs = `${remainingTime.seconds}`.padStart(2, '0');
 
-    switchMode(mode)
+  let minutes = document.getElementById('js-minutes');
+  let seconds = document.getElementById('js-seconds');
+
+  minutes.textContent = remainingMins;
+  seconds.textContent = remainingSecs;
+
+  const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+  document.title = `${remainingMins}:${remainingSecs} - ${text}`
+
+  const progress = document.getElementById('js-progress');
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total
+}
+
+let interval;
+const startTimer = () => {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
+
+  if(timer.mode === 'pomodoro') timer.sessions++;
+
+  mainButton.dataset.action = 'stop';
+  mainButton.textContent = 'stop';
+  mainButton.classList.add('active');
+
+  interval = setInterval(() => {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateTimer()
+
+    total = timer.remainingTime.total;
+
+    if(total <= 0){
+      clearInterval(interval);
+
+      switch(timer.mode){
+        case 'pomodoro':
+          if(timer.sessions % timer.longBreakInterval === 0){
+            switchMode("longBreak");
+          }else{
+            switchMode("shortBreak");
+          }
+          break;
+        default:
+          switchMode('pomodoro')
+      }
+
+      document.querySelector(`[data-sound="${timer.mode}"]`).play()
+
+      if(Notification.permission === 'granted'){
+        const text = (timer.mode === 'pomodoro') ? 
+          'Get back to work!' : 'Take a break!';
+        
+          new Notification(text)
+      }
+
+      startTimer()
+    }
+  }, 1000);
+}
+
+const stopTimer = () => {
+  clearInterval(interval);
+
+  mainButton.dataset.action = 'start';
+  mainButton.textContent = 'start';
+  mainButton.classList.remove('active');
+}
+
+const getRemainingTime = (endTime) => {
+  const currentTime = Date.parse(new Date());
+  const difference = endTime - currentTime;
+
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
+
+  return {
+    total,
+    minutes,
+    seconds
+  }
+}
+
+const mainButton = document.getElementById('main-button');
+mainButton.addEventListener('click', () => {
+  sounds.play();
+
+  const { action } = mainButton.dataset;
+  if(action === 'js-action'){
+    startTimer()
+  }else{
     stopTimer()
   }
+})
 
-  function switchMode(mode){
-    TIMER.mode = mode;
-    TIMER.remainingTime = {
-      total: TIMER[mode] * 60,
-      minutes: TIMER[mode],
-      seconds: 0
-    }
-
-    document.querySelectorAll('button[data-mode]')
-      .forEach(e => e.classList.remove('active'));
-    
-    document.querySelector(`[data-mode='${mode}']`)
-      .classList.add('active');
-    
-    document.body.style.backgroundColor = `var(--${mode})`;
-    document.body.style.color = `var(--${mode === 'pomodoro' ? 'white' : 'dark'})`;
-    document.getElementById('js-progress')
-      .setAttribute('max', TIMER.remainingTime.total);
-    
-    updateTimer()
-  }
-
-  function updateTimer(){
-    const { REMAINING_TIME } = TIMER;
-    const REMAINING_MINS = `${REMAINING_TIME.minutes}`.padStart(2, '0');
-    const REMAINING_SECS = `${REMAINING_TIME.seconds}`.padStart(2, '0');
-
-    const MINUTES = document.getElementById('js-minutes');
-    const SECONDS = document.getElementById('js-seconds');
-
-    MINUTES.textContent = REMAINING_MINS;
-    SECONDS.textContent = REMAINING_SECS;
-
-    const TEXT = (TIMER.mode === 'pomodoro') ? 'Get back to work!' : 'Take a break';
-    document.title = `${MINUTES}:${SECONDS} - ${TEXT}`;
-
-    const PROGRESS = document.getElementById('js-progress');
-    PROGRESS.value = TIMER[TIMER.mode] * 60 - TIMER.remainingTime.total;
-  }
-
-  let interval;
-  function startTimer(){
-    let { total } = TIMER.remainingTime;
-    const endTime = Date.parse(new Date()) + total * 1000;
-
-    if(TIMER.mode === 'pomodoro') TIMER.sessions++;
-
-    mainButton.dataset.action = 'stop';
-    mainButton.textContent = 'stop';
-    mainButton.classList.add('active');
-
-    interval = setInterval(() => {
-      TIMER.remainingTime = getRemainingTime(endTime);
-      updateTimer();
-
-      total = TIMER.remainingTime.total;
-
-      if(total <= 0){
-        clearInterval(interval);
-
-        switch(TIMER.mode){
-          case 'pomodoro':
-            if(TIMER.sessions % TIMER.longBreakInterval === 0){
-              switchMode('longBreak')
-            }else{
-              switchMode('shortBreak')
-            }
-            break;
-          default:
-            switchMode('pomodoro')
-        }
-
-        if(Notification.permission === 'granted'){
-          const text = TIMER.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
-          new Notification(text)
-        }
-
-        stopTimer()
-      }
-    }, 1000)
-  }
-
-  function stopTimer(){
-    clearInterval(interval);
-
-    mainButton.dataset.action = 'start';
-    mainButton.textContent = 'start';
-    mainButton.classList.remove('active')
-  }
-
-  function getRemainingTime(endTime){
-    const CURRENT_TIME = Date.parse(new Date());
-    const DIFFERENCE = endTime - CURRENT_TIME;
-
-    const TOTAL = Number.parseInt(DIFFERENCE / 1000, 10);
-    const MINUTES = Number.parseInt((TOTAL / 60) % 60, 10);
-    const SECONDS = Number.parseInt(TOTAL % 60, 10);
-
-    return {
-      TOTAL,
-      MINUTES,
-      SECONDS
-    }
-  }
-
-  const mainButton = document.getElementById('main-button');
-  mainButton.addEventListener('click', () => {
-    const { action } = mainButton.dataset;
-
-    if(action === 'js-action'){
-      startTimer()
-    }else{
-      stopTimer()
-    }
-  })
+const initApp = () => {
+  switchMode("pomodoro");
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
